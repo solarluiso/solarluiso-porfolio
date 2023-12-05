02d7517040c2313e9152a74978b045bdd026937e
Luis <solarluiso@gmail.com>
2023-12-05 14:10:15 -0800

    styled and finished navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,34 +8,55 @@ const Navbar = () => {
   return (
     <nav className="app__navbar fixed z-[2] flex w-full items-center justify-between border border-solid border-[rgba(255,255,255,0.18)] bg-[rgba(255,255,255,0.25)] px-8 py-4 text-white backdrop-blur-sm">
       <div className="app__navbar-logo flex items-center justify-start">
-        <h1 className="text-xl font-bold">Luiso</h1>
+        <h1 className="font-medium uppercase text-[#b7b1a8]">Luiso</h1>
       </div>
-      <ul className="app__navbar-links">
+      <ul className="app__navbar-links flex flex-1 list-none items-center justify-center max-md:hidden">
         {["hero", "about", "skills", "projects", "contact"].map((item) => (
-          <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
+          <li
+            key={`link-${item}`}
+            className="app__list group mx-4 my-0 flex cursor-pointer flex-col items-center "
+          >
+            <div className="mb-[5px] h-[5px] w-[5px] rounded-[50%] bg-transparent group-hover:bg-[#86e8d2]" />
+            <a
+              href={`#${item}`}
+              className="flex-col font-medium uppercase text-[#b7b1a8] no-underline transition-all duration-[0.3s] ease-[ease-in-out] hover:text-[#86e8d2]"
+            >
+              {item}
+            </a>
           </li>
         ))}
       </ul>
 
-      <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
+      <div className="app__navbar-menu relative flex h-[35px] w-[35px] items-center justify-center rounded-[50%] bg-[#b7b1a8] md:hidden">
+        <HiMenuAlt4
+          onClick={() => setToggle(true)}
+          className="h-[70%] w-[70%] text-white"
+        />
 
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
             transition={{ duration: 0.85, ease: "easeOut" }}
+            className="fixed inset-y-0 right-0 z-[5] flex h-screen w-4/5 flex-col items-end justify-end bg-white bg-cover bg-repeat p-4 shadow-[0px_0px_20px_rgba(168,168,168,0.15)] md:hidden"
           >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {["home", "about", "work", "skills", "contact"].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
+            <HiX
+              onClick={() => setToggle(false)}
+              className="mx-4 my-2 h-[35px] w-[35px] text-[#86e8d2]"
+            />
+            <ul className="m-0 flex h-full w-full list-none flex-col items-start justify-start p-0">
+              {["hero", "about", "skills", "projects", "contact"].map(
+                (item) => (
+                  <li key={item} className="m-4">
+                    <a
+                      href={`#${item}`}
+                      onClick={() => setToggle(false)}
+                      className="text-base font-medium uppercase text-[#b7b1a8] no-underline transition-all duration-[0.3s] ease-[ease-in-out] hover:text-[#86e2d2]"
+                    >
+                      {item}
+                    </a>
+                  </li>
+                ),
+              )}
             </ul>
           </motion.div>
         )}
@@ -45,23 +66,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-{
-  /* <div className="container flex items-center justify-between">
-  <h1 className="text-xl font-bold">Luiso</h1>
-  <ul className="flex space-x-4">
-    <li>
-      <a href="#hero">Home</a>
-    </li>
-    <li>
-      <a href="#about">About</a>
-    </li>
-    <li>
-      <a href="#projects">Projects</a>
-    </li>
-    <li>
-      <a href="#skills">Skills</a>
-    </li>
-  </ul>
-</div> */
-}
